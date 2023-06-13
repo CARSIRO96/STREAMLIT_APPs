75c52a1acd0f8393e4a0d0a98382dac07f987b03
CARSIRO96 <70864434+CARSIRO96@users.noreply.github.com>
2023-06-13 21:01:20 +0200

    CREAR LISTA DE OPCIONES Y DEVOLVER LA INFO SELECCIÓN EN LA APP

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit as st
 
 st.title('My Streamlit App')
 st.write('Welcome to my app!')
-st.write('HA ACTUALIZADO?✅')
+
+options = ['APPLE', 'GOOGLE', 'MICROSOFT']
+selected_option = st.selectbox('Select an option', options)
+
+if st.button('Submit'):
+    st.write('You selected:', selected_option)
