254851977e87f6c729c51b9ff6cb8b9e475b58fa
CARSIRO96 <70864434+CARSIRO96@users.noreply.github.com>
2023-12-10 01:48:09 +0000

    FastAPI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
-import streamlit as st
+try:
+    import streamlit as st
+    import os
+    from fastapi import FastAPI 
+except:
+    os.system('pip install -r requirements.txt')
 
 st.title('My Streamlit App')
 st.write('Welcome to my app!')
@@ -11,7 +16,6 @@ if st.button('Submit'):
     if selected_option == 'CAROL': st.write('CAROL ES LA CANARIA MÁS GUAPA, ME IRÍA DE VIAJE 1 AÑO CON ELLA💛💛💛')
     elif selected_option == 'CHRISTIAN': st.write('QUIERES VIVIR CONMIGO ESTE VERANO Y HACER EL CONEJITO TODOS LOS DIAS??🦔💘🦝 ')
 
-from fastapi import FastAPI 
 app =FastAPI()
  #https://ominous-enigma-6w7645wqq44f4gw4-8000.app.github.dev/ 
 @app.get('/intro') 
